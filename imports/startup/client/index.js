b6e543b4d7cae89b4ee58b71009db1bc1483b912
Amir Mahmoud <amirebrahimi5@live.com>
2018-06-11 05:37:19 +0430

    End of video #6

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-
+import { HttpLink } from 'apollo-link-http';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
 import App from '../../ui/App';
 
+const httpLink = new HttpLink({
+    uri: Meteor.absoluteUrl('graphql')
+});
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+    link: httpLink,
+    cache
+});
+
+const ApolloApp = () => (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
+
 Meteor.startup(() => {
-    render(<App />, document.getElementById("app"));
-})
+    render(<ApolloApp />, document.getElementById("app"));
+});
