ab918b294d586656ead8b4e6ecafd3b1a1b2185c
Amir Mahmoud <amirebrahimi5@live.com>
2018-06-11 05:13:11 +0430

    End of Video #5

---
+++ imports/startup/server/index.js
@@ -0,0 +1,21 @@
+import { createApolloServer } from 'meteor/apollo';
+import { makeExecutableSchema } from 'graphql-tools';
+
+const typeDefs = `
+type Query {
+    hi: String
+}
+`;
+
+const resolvers = {
+    Query: {
+        hi() {
+            return "Hello Level Up";
+        }
+    }
+};
+
+const schema = makeExecutableSchema({
+    typeDefs,resolvers
+})
+createApolloServer({schema});
